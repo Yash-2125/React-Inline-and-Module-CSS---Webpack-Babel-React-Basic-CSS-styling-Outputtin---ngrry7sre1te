638f741bd6c71bf8b298c96be7f9317723667543
Yash Savaliya <80917530+Yash-2125@users.noreply.github.com>
2024-03-26 09:18:55 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,14 @@
-import React from 'react'
-import '../styles/App.css';
-import styles from '../styles/appStyles.module.css';
-const App = () => {
-const heading ={
+import React from "react";
+import styles from "./appStyles.module.css";
 
-
-}
+function InlineModuleCSSFunc() {
+  const heading = { fontSize: "50px", color: "red" };
   return (
     <div id="main">
-      <h1 >Success</h1>
-      <h1 id='error' >Error</h1>
+      <h1 className={styles.success}>Success</h1>
+      <h1 style={heading}>Error</h1>
     </div>
-  )
+  );
 }
 
-
-export default App;
+export default InlineModuleCSSFunc;
